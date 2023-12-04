e16481f2386dc07f546b4dd94f9ada0a254adf94
Harmeet Matharoo <hmjatt@gmail.com>
2023-12-04 08:30:55 -0500

    can see saved word's definition

--- app/src/main/java/algonquin/cst2335/androidfinalproject/IO_dictionary/ui/fragments/SavedWordDefinitionFragment.java
+++ app/src/main/java/algonquin/cst2335/androidfinalproject/IO_dictionary/ui/fragments/SavedWordDefinitionFragment.java
@@ -1,6 +1,7 @@
 package algonquin.cst2335.androidfinalproject.IO_dictionary.ui.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,16 +14,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import algonquin.cst2335.androidfinalproject.IO_dictionary.model.Definition;
+import algonquin.cst2335.androidfinalproject.IO_dictionary.model.DummyData;
 import algonquin.cst2335.androidfinalproject.IO_dictionary.model.SavedWord;
+import algonquin.cst2335.androidfinalproject.IO_dictionary.model.Word;
 import algonquin.cst2335.androidfinalproject.IO_dictionary.ui.adapters.DefinitionsAdapter;
 import algonquin.cst2335.androidfinalproject.R;
-
 public class SavedWordDefinitionFragment extends Fragment {
 
     public static final String ARG_SAVED_WORD_ID = "saved_word_id";
 
     private int savedWordId;
-    private TextView tvSavedWord;
+    private TextView tvSavedWordDetail;
     private RecyclerView recyclerView;
     private DefinitionsAdapter definitionsAdapter;
 
@@ -30,33 +32,39 @@ public class SavedWordDefinitionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.io_recycleview_dictionary, container, false);
 
-        View fragmentDefinition = inflater.inflate(R.layout.io_fragment_word_definition, container, false);
+        View fragmentDefinition = inflater.inflate(R.layout.io_fragment_saved_word_definition, container, false);
 
-
-        // Get the saved word ID from arguments
-        Bundle arguments = getArguments();
-        if (arguments != null && arguments.containsKey(ARG_SAVED_WORD_ID)) {
-            savedWordId = arguments.getInt(ARG_SAVED_WORD_ID);
-        }
 
         // Initialize views
-        tvSavedWord = fragmentDefinition.findViewById(R.id.tvWordDetail);
         recyclerView = view.findViewById(R.id.dictionaryRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Use DummyData class to get dummy definitions
+        List<Definition> dummyDefinitions = DummyData.getDummySavedWordDefinitions();
+
+        // Retrieve the selected word from arguments
+//        Word selectedSavedWord = getArguments().getParcelable(ARG_SAVED_WORD_ID);
+
+        // Retrieve the saved word ID from arguments
+//        savedWordId = getArguments().getInt(ARG_SAVED_WORD_ID);
+
+        // Initialize and set up the DefinitionsAdapter
+        definitionsAdapter = new DefinitionsAdapter(dummyDefinitions);
+        recyclerView.setAdapter(definitionsAdapter);
+
+        // Logging the dummy definitions
+        for (Definition definition : dummyDefinitions) {
+            Log.d("DummySavedData", "Dummy Saved Definition: " + definition.getDefinition());
+        }
+
+        tvSavedWordDetail = fragmentDefinition.findViewById(R.id.tvSavedWordDetail);
 
         // Use the saved word ID to retrieve the saved word from the database
         SavedWord savedWord = getSavedWordById(savedWordId);
 
         if (savedWord != null) {
             // Display the saved word in the TextView
-            tvSavedWord.setText(savedWord.getSavedWord());
-
-            // Use the saved word ID to retrieve the definitions associated with it
-            List<Definition> savedDefinitions = getDefinitionsForSavedWord(savedWordId);
-
-            // Initialize and set up the DefinitionsAdapter
-            definitionsAdapter = new DefinitionsAdapter(savedDefinitions);
-            recyclerView.setAdapter(definitionsAdapter);
+            tvSavedWordDetail.setText(savedWord.getSavedWord());
         }
 
         return view;
@@ -66,12 +74,6 @@ public class SavedWordDefinitionFragment extends Fragment {
     private SavedWord getSavedWordById(int savedWordId) {
         // Query the database using the saved word ID
         // Use the SavedWordDao to retrieve the saved word by ID
-        return null; // Replace with actual database query
-    }
-
-    private List<Definition> getDefinitionsForSavedWord(int savedWordId) {
-        // Query the database using the saved word ID
-        // Use the DefinitionDao or a similar interface to retrieve the definitions associated with the saved word
         return null; // Replace with actual database query
     }
 }
