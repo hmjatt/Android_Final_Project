2e89564e6cb5ba84eb70569dfff1ee659b3d615e
Iuliia <obuk0001@algonquinlive.com>
2023-12-08 21:16:33 -0500

    fixed missing packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,11 +36,11 @@ android {
 dependencies {
 
     implementation("androidx.room:room-common:2.6.1")
-    val room_version = "2.6.0"
+    val room_version = "2.6.1"
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
 
-    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.google.code.gson:gson:2.9.0")
 
 
     implementation("androidx.recyclerview:recyclerview:1.3.2")
@@ -53,14 +53,11 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation("com.android.volley:volley:1.2.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-implementation("com.squareup.picasso:picasso:2.71828")
 
 
 
